eb5984982091f102c58098258efa777adc2a2a47
colabdaegu <colab.daegu@gmail.com>
2025-12-28 07:23:22 +0900

    샘플 캐릭터 수정

--- ngrok-address.js
+++ ngrok-address.js
@@ -1,5 +1,5 @@
-/** ngrok 주소 설정 **/
-const BASE_URL = "https://wilson-unscented-dissidently.ngrok-free.dev";
+/** ngrok 주소 설정(주소 마지막에 / <- 슬래시 넣지 말 것) **/
+const BASE_URL = "https://nondeflected-unbeneficial-jimmie.ngrok-free.dev";
 
 window.NGROK_CONFIG = { 
     NGROK_ADDRESS: BASE_URL + "/chat" 
